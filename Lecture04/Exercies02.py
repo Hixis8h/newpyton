6d214fce7a813c7078ed631c657cf2fa8ac812a5
chayawat <jaokhuneyesmint1@gmail.com>
2026-07-21 16:31:20 +0700

    Lecture04

---
+++ Lecture04/Exercies02.py
@@ -0,0 +1,5 @@
+columns = int(input("How many colums?:"))
+for i in range(100):
+    for j in range(1,1,1):
+        print(i,end="")
+    print()
--- Lecture04/Exercies03.py
+++ Lecture04/Exercies03.py
@@ -1,2 +1,6 @@
+columns = int(input("How many colums?:"))
 for i in range(1,101):
-    print(i)
+    print(f"{i:>3}",end="")
+    for j in range(columns,):
+        if i % columns == 0:
+            print()
