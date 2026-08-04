ff0c6a2ec4bf09442251f3a36c6381aadfb4573c
chayawat <jaokhuneyesmint1@gmail.com>
2026-08-04 16:23:11 +0700

    Lecture05

---
+++ Homework/homework.py
@@ -0,0 +1,7 @@
+def formalt_string(*args):
+    return ''.join(args).upper()
+result = format_strings("Hello","World","this","a","test")
+print(result)
+result = formalt_string ()
+result = formalt_string("concatenate","these","strings","please")
+print(result)
